6060b49dbaa2c243d1db37e45432e8e1d8640908
homindai <129465307+homindai@users.noreply.github.com>
2024-01-26 17:02:47 +0700

    Add files via upload

---
+++ bai1.php
@@ -0,0 +1,4 @@
+<?php
+$n = readline("Nhập vào số nguyên n: ");
+echo "Giá trị tuyệt đối của $n là " . abs($n);
+?>
---
+++ bai6.php
@@ -0,0 +1,5 @@
+<?php
+$n = readline("Nhập vào số nguyên dương N có 2 chữ số: ");
+$sum = floor($n / 10) + $n % 10;
+echo "Tổng các chữ số của $n là $sum";
+?>
